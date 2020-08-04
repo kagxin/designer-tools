b62fd77654a5ff104cbd487e68a5a6256b771f85
康鑫 <k506855660@163.com>
2020-08-04 23:29:59 +0800

    mktxt文件名添加图片高度

--- mktxt/main.go
+++ mktxt/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -10,18 +14,36 @@ import (
 
 func main() {
 	var count = 0
+	var im image.Config
+	var decode func(r io.Reader) (image.Config, error)
 	files, err := ioutil.ReadDir(".")
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, f := range files {
-		if f.Name() == "main.go" || f.Name() == "mktxt.exe" {
+		if strings.HasSuffix(f.Name(), "png") {
+			decode = png.DecodeConfig
+		} else if strings.HasSuffix(f.Name(), "jpg") {
+			decode = jpeg.DecodeConfig
+		} else {
 			continue
 		}
-		fileName := fmt.Sprintf("%s.txt", strings.Split(f.Name(), ".")[0])
-		fmt.Println(fileName)
-		os.OpenFile(fileName, os.O_RDONLY|os.O_CREATE, 0666)
-		count++
+		if reader, err := os.Open(f.Name()); err == nil {
+			defer reader.Close()
+			im, err = decode(reader)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s: %v\n", f.Name(), err)
+				continue
+			}
+			fileName := fmt.Sprintf("%s-%d.txt", strings.Split(f.Name(), ".")[0], im.Height)
+			fmt.Println(fileName)
+			if f, err := os.OpenFile(fileName, os.O_RDONLY|os.O_CREATE, 0666); err != nil {
+				defer f.Close()
+			}
+			count++
+		} else {
+			fmt.Printf("Impossible to open the file: %s, err:%#v\n", f.Name(), err)
+		}
 	}
 	fmt.Printf("完成，共创建%d个txt\n", count)
 	var ts string
